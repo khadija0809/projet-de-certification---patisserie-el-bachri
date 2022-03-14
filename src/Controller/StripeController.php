115ce04c4ab260b72b0a58ecc323afef7324bf79
khadija bahi <khadiija1bahi@gmail.com>
2022-03-14 19:41:39 +0100

    modification

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -22,7 +22,7 @@ class StripeController extends AbstractController
     public function index(EntityManagerInterface $entityManager, Cart $cart, $reference)
     {
         $products_for_stripe = [];
-        $YOUR_DOMAIN = 'http://127.0.0.1:8000';
+        $YOUR_DOMAIN = 'https://www.patisserie-elbachri.com/';
 
        $order = $entityManager->getRepository(Order::class)->findOneByReference($reference);
         
